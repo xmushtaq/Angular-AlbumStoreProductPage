96f65db920c0fac340aefbfd455ed97af023817e
PKRMSEZDC\mmushtaq <xmushtaq@gmail.com>
2018-12-07 19:39:29 +0530

    added product-tracklisting component

---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,26 @@
+import { Album } from '../album';
+import { Track } from '../track';
+import { ProductService } from '../product.service';
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
+})
+export class ProductTracklistingComponent implements OnInit {
+
+  albumInfo: Album;
+
+  constructor(private _productService: ProductService) { }
+
+  ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => {
+          this.albumInfo = response;
+          // console.log(response['album']);
+          // console.log(this.albumInfo['album'].tracks);
+      });
+  }
+
+}
